b0c3d4d2f948883fee1ee290a9072ad65b145996
dave <oblivion.davecave@gmail.com>
2015-05-31 00:41:16 -0300

    Priority scheduling. All greatest priority process execute in roundrobin.

--- algorithms/priority.c
+++ algorithms/priority.c
@@ -15,9 +15,9 @@ void init_scheduler(struct process* list, int size, int ramend) {
     list[3].task_pointer = task3;
 
     list[0].priority = 0;
-    list[1].priority = 1;
-    list[2].priority = 2;
-    list[3].priority = 3;
+    list[1].priority = 11;
+    list[2].priority = 11;
+    list[3].priority = 12;
 
     list[0].stack_pointer = (int) ramend-100;
 
@@ -32,23 +32,42 @@ void init_scheduler(struct process* list, int size, int ramend) {
         *px = (char) (((int) list[i].task_pointer) >> 8);
     }
 
-    for(i=0; i < size; i++) {
+    for(i=1; i < size; i++) {
         list[i].stack_pointer-=35;
     }
 }
 
 int next_task(struct process* list, int size) {
 
+    ///*Variáveis que armazenarão o valor da maior prioridade
+       encontrada entre os processos e o índice do processo
+       com com maior prioridade */
     int greatest_priority = 0;
     int greatest_priority_index = 0;
 
+    /*Busca pelo processo que estava sendo executado logo
+    antes da interrupção*/
     int i;
+    for(i=0; i < size; i++)
+        if(list[i].running == 1) break;
+    int currently_running = i;
+    if(currently_running < size)
+        list[currently_running].running = 0;
+
+    /*Busca pelo processo com maior prioridade dentro da
+    lista de processos. Se houver mais de um processo com
+    o mesmo nível de prioridade, eles serão executados de
+    forma circular.*/
     for(i=0; i < size; i++) {
-        if(list[i].priority > greatest_priority) {
-            greatest_priority = list[i].priority;
-            greatest_priority_index = i;
+        int j = (i + currently_running + 1) % size;
+        if(list[j].priority > greatest_priority) {
+            greatest_priority = list[j].priority;
+            greatest_priority_index = j;
         }
     }
 
+    /*Declara o próximo processo a ser executado e retorna
+    seu índice*/
+    list[greatest_priority_index].running = 1;
     return greatest_priority_index;
 }
